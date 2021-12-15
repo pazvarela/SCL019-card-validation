e9249c15f113eda1bb0160b637d19e0760fc8825
pazvarela <89282860+pazvarela@users.noreply.github.com>
2021-12-15 15:45:58 -0400

    terminé el form y agregué información al footer y header. En css le di estilo al html y en javascript realicé una función para cambiar el color del submit

--- src/validator.js
+++ src/validator.js
@@ -1,3 +1,7 @@
+let btnTarjeta = document.querySelector('#agregaTarjeta');
+
+btnTarjeta.addEventListener('click', () => btnTarjeta.style.backgroundColor = '#4cae4c')
+
 const validator = {
   // ...
 };
