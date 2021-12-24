56da55a0692da46aa337872973696a92f4c88e2c
pazvarela <89282860+pazvarela@users.noreply.github.com>
2021-12-24 13:43:35 -0400

    realicé la función validación tarjeta de crédito y está cumpliendo con su funcionalidad correctamente

--- src/validator.js
+++ src/validator.js
@@ -1,9 +1,49 @@
-let btnTarjeta = document.querySelector('#agregaTarjeta');
-
-btnTarjeta.addEventListener('click', () => btnTarjeta.style.backgroundColor = '#4cae4c')
-
 const validator = {
-  // ...
-};
+  // crear metodo o funcion para validar listo
+  // convertir en array el string listo
+  // convertir a numero las posiciones listooo
+  // girar el array listo
+  // recorrer array listo
+  // buscar posiciones pares listo
+  // multiplicar posiciones pares listo
+  // ver resultado si es mas de 9 sumar ambos digitos listo
+  // guardar tambien en algun lugar los numeros de las posiciones impares
+  // sumar todos los numero
+  // buscar % de 10
+  isValid: function(cardNumber) {
+  /*console.log("hola")*/
+  let arrayValidation = cardNumber.split('').reverse().map(Number);
+  /*console.log(arrayValidation);*/
+  let total = 0
+  for (let i = 0; i < arrayValidation.length; i++) {
+    if (i % 2 === 1) {
+      /*console.log(i) /*posición*/
+      /*console.log(arrayValidation[i]) /*el número en la posición*/
+      /*console.log(arrayValidation[i]*2) /*el número en la posición par *2 */
+      /*variable para multiplicar la posición par *2 */
+      let doublenum = arrayValidation[i] * 2
+      /*console.log(doublenum)*/
+      if (doublenum >= 10) {
+        let sum = doublenum - 9
+        total += sum
+        /*console.log(total)*/
+      } else {
+        total += doublenum;
+      }
+    
+    } else {
+      total += arrayValidation [i];
+      
+    }
+  } 
+  /*console.log(total)*/
+  let result = total % 10
+  if (result == 0) {
+    return true
+  } else {
+    return false
+  }
+}
+}
 
-export default validator;
+export default validator; 
