aadfb9b1417adae36e5ce1183b3a53f27ac85fda
pazvarela <89282860+pazvarela@users.noreply.github.com>
2021-12-24 13:42:00 -0400

    realicé cambios en el dom para tomar el valor del input y tener el resultado cuando la tarjeta sea valida o invalida

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import validator from './validator.js';
 
 console.log(validator);
+/* variable para traer los datos del input */
+let btnTarjeta = document.querySelector('#agregaTarjeta');
+
+  btnTarjeta.addEventListener('click', () => {
+  btnTarjeta.style.backgroundColor = '#4cae4c'
+  let inputCardNumber = document.querySelector('#cardNumber').value; /* Va a buscar el input con el valor */
+  /*console.log(inputCardNumber.value)*/
+  const result = validator.isValid(inputCardNumber)
+  console.log(result)
+
+  })
+  
+
